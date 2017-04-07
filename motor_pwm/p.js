8b957f5d6ebdb2625f3500e229db7c5316784b03
Omkar Dusane <omkardusane@gmail.com>
2017-04-07 11:19:09 +0000

    dr

--- motor_pwm/p.js
+++ motor_pwm/p.js
@@ -2,16 +2,36 @@ const raspi = require('raspi');
 const PWM = require('raspi-pwm').PWM;
 console.log('start now')
 let speed = 9 ;
-if(process.argv[2]){
+let pin = 1 ;
+if(process.argv[2] && process.argv[3]){
     speed = Number(process.argv[2])
-  }
+    pin = Number(process.argv[3])
+    console.log('Speed ',speed);
+    raspi.init(() => {
+      console.log('init')
+      let pinStr = 'GPIO18';
+      switch(pin){
+        case 0:
+          pinStr = 'GPIO18';
+          break;
+        case 1:
+          pinStr = 'GPIO17';
+          break;
+        case 2:
+          pinStr = 'GPIO19';
+          break;
+        case 3:
+          pinStr = 'GPIO12';
+          break;
+        case 4:
+          pinStr = 'GPIO13';
+          break;
+      }
+      const pwm = new PWM(pinStr);
+      pwm.write(speed); // Center a servo
+      console.log('speed On ',pinStr," : ",speed);
+      
+    });
+}
 // GPIO18 = pin 7 
 
-console.log('Speed ',speed);
-raspi.init(() => {
-  console.log('init')
-  const pwm = new PWM('GPIO18');
-  pwm.write(speed); // Center a servo
-  console.log('wrote');
-  
-});
