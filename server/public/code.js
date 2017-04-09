214a6a26f9c0d8a9c028099cdbd99b0007243e02
apu2706 <apu2706@gmail.com>
2017-04-09 22:14:46 +0530

    Added new controls to the panel.

--- server/public/code.js
+++ server/public/code.js
@@ -65,35 +65,8 @@ app.controller('gyroCtrl',function($scope){
 app.directive("mdNumPicker", function(){
 return {
           restrict: 'EC',
-          controller: 'MotorControlCtrl',
-          scope: {
-            val: '=ngModel',
-            maxValue: '=*?',
-            minValue: '=*?',
-            onChange: '&',
-            name : '@'        
-          },
-          template: [
-            '<div layout="column" layout-align="center stretch">',
-                '<md-button class="md-raised" ng-click="incVal()" ng-disabled="isMaxValue()">+</md-button>',
-                '<div class="md-num-picker__content md-display-1" ng-class="{\'animate-up\': animateUp}">',
-                  '<div ng-animate-swap="val" class="md-num-picker__content-view">{{val}}</div>',
-                '</div>',
-                '<md-button class="md-raised" ng-click="decVal()" ng-disabled="isMinValue()">-</md-button>',
-              '</div>'].join(' ').replace(/\s+/g, ' ')
-        }
-
-})
-var socket = io.connect(window.location.host);
-app.controller('MotorControlCtrl',function($scope){
-    console.log('Motor Controller Loaded');
-
-            $scope.sendValue = (motorNumber,val)=>{
-                console.log({value : val});
-                socket.emit('speed-motor', {payload:{motorNumber : motorNumber, value : val},message:'speed change'});
-            }    
-
-
+          controller: function($scope){
+            console.log('Loaded directive Ctrl')
             $scope.stop = ()=>{
                // $scope.state.started= false;
                 socket.emit('stop-motor', {message:'stop motor'});
@@ -104,8 +77,8 @@ app.controller('MotorControlCtrl',function($scope){
                 if(!angular.isUndefined($scope.maxValue) && $scope.val>=$scope.maxValue){
                   return;
                 };
-                $scope.val++;
-                $scope.sendValue($scope.name,$scope.val);
+                $scope.val = $scope.val + parseInt($scope.$parent.deltaValue);
+                $scope.$parent.sendValue($scope.name,$scope.val);
                 if(!!$scope.onChange){
                   $scope.onChange({value: $scope.val})
                 }
@@ -117,8 +90,8 @@ app.controller('MotorControlCtrl',function($scope){
                 if(!angular.isUndefined($scope.minValue) && $scope.val<=$scope.minValue){
                   return;
                 };
-                $scope.val--;
-                $scope.sendValue($scope.name,$scope.val);
+                $scope.val = $scope.val - parseInt($scope.$parent.deltaValue);
+                $scope.$parent.sendValue($scope.name,$scope.val);
                 if(!!$scope.onChange){
                   $scope.onChange({value: $scope.val})
                 }
@@ -145,7 +118,74 @@ app.controller('MotorControlCtrl',function($scope){
             
             $scope.$watch('val', function(newVal, oldVal){
               $scope.animateUp = newVal < oldVal ; 
-            })
+            });
+
+          },
+          scope: {
+            val: '=ngModel',
+            maxValue: '=*?',
+            minValue: '=*?',
+            onChange: '&',
+            name : '@',   
+          },
+          template: [
+            '<div layout="column" layout-align="center stretch">',
+                '<md-button class="md-raised" ng-click="incVal()" ng-disabled="isMaxValue()">+</md-button>',
+                '<div class="md-num-picker__content md-display-1" ng-class="{\'animate-up\': animateUp}">',
+                  '<div ng-animate-swap="val" class="md-num-picker__content-view">{{val}}</div>',
+                '</div>',
+                '<md-button class="md-raised" ng-click="decVal()" ng-disabled="isMinValue()">-</md-button>',
+              '</div>'].join(' ').replace(/\s+/g, ' ')
+        }
+})
+var socket = io.connect(window.location.host);
+app.controller('MotorControlCtrl',function($scope){
+
+
+
+            $scope.deltaValue = 1;
+
+            $scope.globalValueApply = function(){
+                $scope.value1 = $scope.value2 = $scope.value3 = $scope.value4 = $scope.globalValue;
+                socket.emit('speed-motor', {payload:{motorNumber : 1, value : $scope.globalValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 2, value : $scope.globalValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 3, value : $scope.globalValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 4, value : $scope.globalValue},message:'speed change'});
+            }
+
+            $scope.globalIncrementer = function(){
+                $scope.dummyDeltaValue = parseInt($scope.deltaValue);
+
+                $scope.value1 = $scope.value1 + $scope.dummyDeltaValue;
+                $scope.value2 = $scope.value2 + $scope.dummyDeltaValue;
+                $scope.value3 = $scope.value3 + $scope.dummyDeltaValue;
+                $scope.value4 = $scope.value4 + $scope.dummyDeltaValue;
+
+                socket.emit('speed-motor', {payload:{motorNumber : 1, value : $scope.value1 + $scope.dummyDeltaValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 2, value : $scope.value2 + $scope.dummyDeltaValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 3, value : $scope.value3 + $scope.dummyDeltaValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 4, value : $scope.value4 + $scope.dummyDeltaValue},message:'speed change'});                
+            }
+
+            $scope.globalDecrementer = function(){
+                $scope.dummyDeltaValue = parseInt($scope.deltaValue);
+                $scope.value1 = $scope.value1 - $scope.dummyDeltaValue;
+                $scope.value2 = $scope.value2 - $scope.dummyDeltaValue;
+                $scope.value3 = $scope.value3 - $scope.dummyDeltaValue;
+                $scope.value4 = $scope.value4 - $scope.dummyDeltaValue;
+
+                socket.emit('speed-motor', {payload:{motorNumber : 1, value : $scope.value1 - $scope.dummyDeltaValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 2, value : $scope.value2 - $scope.dummyDeltaValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 3, value : $scope.value3 - $scope.dummyDeltaValue},message:'speed change'});
+                socket.emit('speed-motor', {payload:{motorNumber : 4, value : $scope.value4 - $scope.dummyDeltaValue},message:'speed change'});                
+            }            
+
+            $scope.sendValue = (motorNumber,val)=>{
+                console.log({value : val});
+                socket.emit('speed-motor', {payload:{motorNumber : motorNumber, value : val},message:'speed change'});
+            }    
+
+
             
 
 });
